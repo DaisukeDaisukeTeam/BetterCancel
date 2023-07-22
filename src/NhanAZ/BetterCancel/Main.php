3efbc9ff5d94d1712bf3a18fe8ccd9ab5a44927e
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2023-07-22 14:21:58 +0900

    update

--- src/NhanAZ/BetterCancel/Main.php
+++ src/NhanAZ/BetterCancel/Main.php
@@ -8,17 +8,27 @@ use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\EventPriority;
 use pocketmine\plugin\PluginBase;
+use pocketmine\world\Position;
 
 class Main extends PluginBase {
 	protected function onEnable(): void {
 		$manager = $this->getServer()->getPluginManager();
 
 		$handler = static function (BlockBreakEvent|BlockPlaceEvent $event): void {
-			if (!$event->isCancelled()) return;
+			//if (!$event->isCancelled()) return;
+
 			$player = $event->getPlayer();
 			$session = $player->getNetworkSession();
 			$session->sendDataPacket(DenySound::getPacket($player->getLocation()));
-			$session->sendDataPacket(ForceFieldParticle::getPacket($event->getBlock()->getPosition()));
+			if($event instanceof BlockBreakEvent){
+				$session->sendDataPacket(ForceFieldParticle::getPacket($event->getBlock()->getPosition()));
+			}else{
+				//assert($event instanceof BlockPlaceEvent);
+				foreach($event->getTransaction()->getBlocks() as [$x, $y, $z, $block]){
+					$session->sendDataPacket(ForceFieldParticle::getPacket(new Position($x, $y, $z, $player->getWorld())));
+				}
+			}
+
 		};
 
 		$manager->registerEvent(
